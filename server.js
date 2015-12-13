7dfc48705a046e0fff3e879b5f93aec72c836b98
Eric Craft <eacraft13@gmail.com>
2015-12-12 23:58:33 -0800

    save to rethinkdb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var _         = require('lodash');
+var async     = require('async');
+var r         = require('rethinkdb');
 var covers    = require('./lib/scrapers/covers');
 var Arbitrage = require('./lib/models/arbitrage');
 
@@ -7,13 +8,21 @@ var Arbitrage = require('./lib/models/arbitrage');
 covers(function(err, data) {
     if (err) throw err;
 
-    _.each(data, function(match) {
-        Arbitrage.calculate(match.board, 1000, function(err, data) {
-            if (data) {
-                console.log(match.sport.toUpperCase(), ':', match.away, 'vs', match.home, ':', match.time);
-                console.log(JSON.stringify(data, null, 4));
-                console.log();
-            }
+    r.connect()
+    .then(function(conn) {
+
+        async.each(data, function(match, callback) {
+            Arbitrage.calculate(match.board, 1000, function(err, data) {
+                if (data) {
+                    console.log(match.sport.toUpperCase(), ':', match.away, 'vs', match.home, ':', match.time);
+                    console.log(JSON.stringify(data, null, 4));
+                    console.log();
+                }
+            });
+            r.table('match').insert(match, { conflict: 'replace' }).run(conn, callback);
+        }, function(err) {
+            conn.close();
         });
+
     });
 });
