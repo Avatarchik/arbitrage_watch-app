a3ae488302357527f5ce71a0061af3a710231be0
Eric Craft <eacraft13@gmail.com>
2015-12-13 00:36:17 -0800

    Auto scraping and calculating

--- server.js
+++ server.js
@@ -4,25 +4,38 @@ var covers    = require('./lib/scrapers/covers');
 var Arbitrage = require('./lib/models/arbitrage');
 
 
+setInterval(function() {
+    process.stdout.write('.');
+    covers(function(err, data) {
+        if (err) throw err;
+        r.connect()
+        .then(function(conn) {
+            async.each(data, function(match, callback) {
+                r.table('match').insert(match, { conflict: 'replace' }).run(conn, callback);
+            }, function(err) {
+                conn.close();
+            });
+        });
+    });
+}, 1000 * 60 * 8); // every 8 minutes
 
-covers(function(err, data) {
-    if (err) throw err;
 
-    r.connect()
-    .then(function(conn) {
-
-        async.each(data, function(match, callback) {
+r.connect()
+.then(function(conn) {
+    r.table('match').changes().run(conn, function(err, cursor) {
+        cursor.each(function(err, row) {
+            var match = row.new_val;
             Arbitrage.calculate(match.board, 1000, function(err, data) {
                 if (data) {
-                    console.log(match.sport.toUpperCase(), ':', match.away, 'vs', match.home, ':', match.time);
-                    console.log(JSON.stringify(data, null, 4));
-                    console.log();
+                    r.table('arbitrage').insert({
+                        sport: match.sport.toUpperCase(),
+                        away: match.away,
+                        home: match.home,
+                        time: match.time,
+                        data: data
+                    });
                 }
             });
-            r.table('match').insert(match, { conflict: 'replace' }).run(conn, callback);
-        }, function(err) {
-            conn.close();
         });
-
     });
 });
